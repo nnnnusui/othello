b28abcd66150f1f9bbe211801a04ee2498ce548c
nnnnusui <nnnnusui@gmail.com>
2019-11-23 16:52:55 +0900

    ➕📝 Operator#-()

--- src/main/scala/com/github/nnnnusui/abstraction/math/operation/Operator.scala
+++ src/main/scala/com/github/nnnnusui/abstraction/math/operation/Operator.scala
@@ -2,7 +2,11 @@ package com.github.nnnnusui.abstraction.math.operation
 
 class Operator[Lhs, Out](lhs: Lhs){
   def +[Rhs](rhs: Rhs)(implicit toLhs: Rhs => Lhs, has: Plus[Lhs, Out]): Out
-  = has.plus(lhs, rhs)
+    = has.plus(lhs, rhs)
   def +(rhs: Lhs)(implicit has: Plus[Lhs, Out]): Out
-  = has.plus(lhs, rhs)
+    = has.plus(lhs, rhs)
+  def -[Rhs](rhs: Rhs)(implicit toLhs: Rhs => Lhs, has: Minus[Lhs, Out]): Out
+    = has.minus(lhs, rhs)
+  def -(rhs: Lhs)(implicit has: Minus[Lhs, Out]): Out
+    = has.minus(lhs, rhs)
 }
