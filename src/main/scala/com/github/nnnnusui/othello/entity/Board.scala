f5f1a06d9a454bc3f3d943ece98a1d719a3bfa5a
nnnnusui <nnnnusui@gmail.com>
2019-11-26 17:13:03 +0900

    ➕ Board#flip() (, PutState, FlipState)

--- src/main/scala/com/github/nnnnusui/othello/entity/Board.scala
+++ src/main/scala/com/github/nnnnusui/othello/entity/Board.scala
@@ -4,7 +4,27 @@ import com.github.nnnnusui.anydimensional.{Coordinates, Space}
 
 object Board {
   private var space = Space[Disc]()
-  def put(disc: Disc, coordinates: Coordinates): Unit ={
+  def put(disc: Disc, coordinates: Coordinates): PutState ={
+    if (space.isDefinedAt(coordinates))
+      return PutState.DiskAlreadyExists
     space = space.updated(coordinates, disc)
+    PutState.Success
   }
+  def flip(disc: Disc, coordinates: Coordinates): FlipState ={
+    if (!space.isDefinedAt(coordinates))
+      return FlipState.DiskDoesNotExists
+    space = space.updated(coordinates, disc)
+    FlipState.Success
+  }
+  def state: Space[Disc] = space
+}
+sealed trait PutState
+object PutState{
+  case object DiskAlreadyExists extends PutState
+  case object Success extends PutState
+}
+sealed trait FlipState
+object FlipState{
+  case object DiskDoesNotExists extends FlipState
+  case object Success extends FlipState
 }
